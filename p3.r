6f38326f0f06bb87eaae90773d108b102edc95ec
nattayamini <150422034+nattayamini@users.noreply.github.com>
2024-06-13 05:00:25 +0530

    Add files via upload

---
+++ p3.r
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <p>I like coding</p>
+                <h2>coding is fun</h2>
+                <p>I started coding in React</p>
+            </div>
+        )
+    }
+}
+export default App;
