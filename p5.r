5deb07c141dfe9f02de338450ddba0e9ae4bf369
nattayamini <150422034+nattayamini@users.noreply.github.com>
2024-06-13 05:16:23 +0530

    Add files via upload

---
+++ p5.r
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>NIPUNA TECHNOLOGIES</h1>
+        <p>REACT JS</p>
+        <p className="demo">Sum={2+2}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
