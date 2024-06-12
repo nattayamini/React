722b840467fb69ef64d7f3494a9f3d536bb20cfb
nattayamini <150422034+nattayamini@users.noreply.github.com>
2024-06-13 05:12:38 +0530

    Add files via upload

---
+++ p4.r
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>NIPUNA TECHNOLOGIES</h1>
+        <p>REACT JS</p>
+        <p data-demo="demo">Vijayawada</p>
+      </div>
+    );
+  }
+}
+
+export default App;
